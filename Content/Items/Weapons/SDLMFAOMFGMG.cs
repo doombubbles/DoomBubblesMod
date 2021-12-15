6e0eadae4d1d92e66986d2106861383b2b47e9ae
compupro <7752184+compupro@users.noreply.github.com>
2021-12-14 20:48:56 -0800

    fix capitalization of SDLMFAOMFGMG

--- Content/Items/Weapons/SDLMFAOMFGMG.cs
+++ Content/Items/Weapons/SDLMFAOMFGMG.cs
@@ -4,7 +4,7 @@ using Terraria.DataStructures;
 
 namespace DoomBubblesMod.Content.Items.Weapons;
 
-public class Sdlmfaomfgmg : ModItem
+public class SDLMFAOMFGMG : ModItem
 {
     public override void SetStaticDefaults()
     {
