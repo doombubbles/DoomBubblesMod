5c266e5ae7be8c0e67822839018b5ab44d174158
compupro <7752184+compupro@users.noreply.github.com>
2021-12-24 00:00:21 -0800

    change eye color depending on context

--- Content/DrawLayers/CompuDrawLayer.cs
+++ Content/DrawLayers/CompuDrawLayer.cs
@@ -1,7 +1,5 @@
 using Terraria.DataStructures;
 using ReLogic.Content;
-using Microsoft.Xna.Framework;
-using DoomBubblesMod.Common.Players;
 using DoomBubblesMod.Content.Items.Armor;
 
 namespace DoomBubblesMod.Content.DrawLayers;
@@ -18,13 +16,25 @@ public class CompuDrawLayer : PlayerDrawLayer
     protected override void Draw(ref PlayerDrawSet drawInfo)
     {
         var player = drawInfo.drawPlayer;
+        if (player.sleeping.isSleeping) return;
+
+        var color = Color.Cyan;
+        if (player.ZoneCrimson) {
+            color = Color.Red;
+        } else if (player.ZoneCorrupt) {
+            color = Color.BlueViolet;
+        } else if (player.ZoneUnderworldHeight) {
+            color = Color.Orange;
+        } else if (player.ZoneHallow) {
+            color = Color.HotPink;
+        }
 
         compuproHeadTexture ??= Request<Texture2D>("DoomBubblesMod/Assets/Textures/CompuproEyes", AssetRequestMode.ImmediateLoad);
         var drawX = (player.width / 2) - (player.bodyFrame.Width / 2) + drawInfo.Position.X - Main.screenPosition.X;
         var drawY = 4 + player.height - player.bodyFrame.Height + drawInfo.Position.Y - Main.screenPosition.Y;
         var drawPosition = new Vector2((int) drawX, (int) drawY) + player.headPosition + drawInfo.headVect + drawInfo.helmetOffset;
 
-        var drawData = new DrawData(compuproHeadTexture.Value, drawPosition, player.bodyFrame, Color.White, player.headRotation, drawInfo.headVect,
+        var drawData = new DrawData(compuproHeadTexture.Value, drawPosition, player.bodyFrame, color, player.headRotation, drawInfo.headVect,
                 1f, drawInfo.playerEffect, 0);
         drawInfo.DrawDataCache.Add(drawData);
     }
