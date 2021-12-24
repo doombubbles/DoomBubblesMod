68d57c629102d3fdb6de589571f0b74661330f2e
compupro <7752184+compupro@users.noreply.github.com>
2021-12-23 22:16:43 -0800

    remove compuGlow field

--- Common/Players/CompuPlayer.cs
+++ Common/Players/CompuPlayer.cs
@@ -6,10 +6,7 @@ namespace DoomBubblesMod.Common.Players;
 
 public class CompuPlayer : ModPlayer
 {
-    public bool showCompuGlow;
-
     public override void ResetEffects()
     {
-        showCompuGlow = false;
     }
 }
--- Content/DrawLayers/CompuDrawLayer.cs
+++ Content/DrawLayers/CompuDrawLayer.cs
@@ -16,18 +16,20 @@ public class CompuDrawLayer : PlayerDrawLayer
 
     protected override void Draw(ref PlayerDrawSet drawInfo)
     {
-        compuproHeadTexture ??= Request<Texture2D>("DoomBubblesMod/Assets/Textures/CompuproEyes", AssetRequestMode.ImmediateLoad);
-        var drawX = (drawInfo.drawPlayer.width / 2) - (drawInfo.drawPlayer.bodyFrame.Width / 2) + drawInfo.Position.X - Main.screenPosition.X;
-        var drawY = 4 + drawInfo.drawPlayer.height - drawInfo.drawPlayer.bodyFrame.Height + drawInfo.Position.Y - Main.screenPosition.Y;
-        var drawPosition = new Vector2((int) drawX, (int) drawY) + drawInfo.drawPlayer.headPosition + drawInfo.headVect + drawInfo.helmetOffset;
+        var player = drawInfo.drawPlayer;
 
-        var drawData = new DrawData(compuproHeadTexture.Value, drawPosition, drawInfo.drawPlayer.bodyFrame, Color.White, drawInfo.drawPlayer.headRotation, drawInfo.headVect,
+        compuproHeadTexture ??= Request<Texture2D>("DoomBubblesMod/Assets/Textures/CompuproEyes", AssetRequestMode.ImmediateLoad);
+        var drawX = (player.width / 2) - (player.bodyFrame.Width / 2) + drawInfo.Position.X - Main.screenPosition.X;
+        var drawY = 4 + player.height - player.bodyFrame.Height + drawInfo.Position.Y - Main.screenPosition.Y;
+        var drawPosition = new Vector2((int) drawX, (int) drawY) + player.headPosition + drawInfo.headVect + drawInfo.helmetOffset;
+
+        var drawData = new DrawData(compuproHeadTexture.Value, drawPosition, player.bodyFrame, Color.White, player.headRotation, drawInfo.headVect,
                 1f, drawInfo.playerEffect, 0);
         drawInfo.DrawDataCache.Add(drawData);
     }
 
     public override bool GetDefaultVisibility(PlayerDrawSet drawInfo)
     {
-        return drawInfo.drawPlayer.GetModPlayer<CompuPlayer>().showCompuGlow;
+        return drawInfo.drawPlayer.head == ModContent.GetModHeadSlot("DoomBubblesMod/Items/Armor/CompuproHead_Head");
     }
 }
